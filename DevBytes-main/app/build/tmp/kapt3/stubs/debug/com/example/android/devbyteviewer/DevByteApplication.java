b5cfd0924b5be42fb61e227a35022a2c0105bf60
Sanda Albrecht <sanda_albrecht@Deniss-MacBook-Pro.local>
2021-09-01 15:46:31 +0300

    Add WorkRequest

--- DevBytes-main/app/build/tmp/kapt3/stubs/debug/com/example/android/devbyteviewer/DevByteApplication.java
+++ DevBytes-main/app/build/tmp/kapt3/stubs/debug/com/example/android/devbyteviewer/DevByteApplication.java
@@ -5,8 +5,9 @@ import java.lang.System;
 /**
  * Override application to setup background work via WorkManager
  */
-@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\u0003\u001a\u00020\u0004H\u0016\u00a8\u0006\u0005"}, d2 = {"Lcom/example/android/devbyteviewer/DevByteApplication;", "Landroid/app/Application;", "()V", "onCreate", "", "app_debug"})
+@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000\u001a\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0003\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\u0005\u001a\u00020\u0006H\u0002J\b\u0010\u0007\u001a\u00020\u0006H\u0016J\b\u0010\b\u001a\u00020\u0006H\u0002R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\t"}, d2 = {"Lcom/example/android/devbyteviewer/DevByteApplication;", "Landroid/app/Application;", "()V", "applicationScope", "Lkotlinx/coroutines/CoroutineScope;", "delayedInit", "", "onCreate", "setupRecurringWork", "app_debug"})
 public final class DevByteApplication extends android.app.Application {
+    private final kotlinx.coroutines.CoroutineScope applicationScope = null;
     
     public DevByteApplication() {
         super();
@@ -20,5 +21,14 @@ public final class DevByteApplication extends android.app.Application {
      */
     @java.lang.Override()
     public void onCreate() {
+    }
+    
+    private final void delayedInit() {
+    }
+    
+    /**
+     * Setup WorkManager background job to 'fetch' new network data daily.
+     */
+    private final void setupRecurringWork() {
     }
 }
